8a840a23251ee06f1073e6e22cb02bf8c40e7414
Emmanuel Dev <161900791+EmmanuelDevC@users.noreply.github.com>
2024-07-06 01:37:31 -1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let count = 0;
+let discount = 10000;
+const decrease = document.getElementById("decrease");
+const counter = document.getElementById('click');
+const buttons = document.querySelectorAll('#button');
+const checks = document.querySelectorAll('#check');
+const checkeds = document.querySelectorAll('#checked');
+
+function change() {
+    count += 1;
+    discount -= 1;
+    counter.innerText = count;
+    decrease.innerText = discount;
+
+    if (discount <= 0) {
+        discount = 0;
+        decrease.innerText = 0;
+    }
+    
+
+
+    // count += 10; // Counting up by 10
+    //     discount += 10;
+    //     counter.innerText = count;
+    //     decrease.innerText = discount;
+}
+
+buttons.forEach(button => button.addEventListener('click', change));
+
+checks.forEach((check, index) => {
+    check.addEventListener('click', function () {
+        checkeds[index].innerHTML = "Checking";
+        setTimeout(() => {
+            checkeds[index].innerHTML = "Task completed";
+            check.style.background = "rgba(128, 0, 128, 0.334)";
+        }, 15000);
+    });
+});
